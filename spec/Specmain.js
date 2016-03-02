86670ec3603096afac1ac01c0fb4f9245747c192
ChenXiyu <xycinus@gmail.com>
2016-03-02 16:09:53 +0800

    feature: format

--- spec/Specmain.js
+++ spec/Specmain.js
@@ -12,3 +12,17 @@ describe("split()",function(){
         expect(expectOutput).toEqual(output)
     });
 });
+describe("format()",function(){
+    it("format numbers to lcd-format",function(){
+        var input = ['3','2','1'];
+        var expectOutput = '._. ._. ... \n._| ._| ..| \n._| |_. ..| \n'
+        var output = format(input)
+        expect(expectOutput).toEqual(output)
+    });
+    it("format numbers to lcd-format",function(){
+        var input = ['9','1','0'];
+        var expectOutput = '._. ... ._. \n|_| ..| |.| \n..| ..| |_| \n'
+        var output = format(input)
+        expect(expectOutput).toEqual(output)
+    });
+});
